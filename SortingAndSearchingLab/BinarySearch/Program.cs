cd85f058f8f012128ef3c26fbc26a6910e6a4d8f
Emilian Ognianov <emilian_89@abv.bg>
2020-01-26 00:53:52 +0200

    Sort and Search lab complete

---
+++ SortingAndSearchingLab/BinarySearch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int key = int.Parse(Console.ReadLine());
+
+            if (input == String.Empty)
+            {
+                return;
+            }
+
+            int[] arr = input
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int index = BinarySearch.IndexOf(arr, key);
+            Console.WriteLine(index);
+
+        }
+    }
+}
---
+++ SortingAndSearchingLab/MergeSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input == String.Empty)
+            {
+                return;
+            }
+
+            int[] arr = input
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Mergesort<int>.Sort(arr);
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+
+}
+
